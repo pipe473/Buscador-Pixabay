0c295a33af24ca774bddf5c585b66a8d186c8fe7
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-19 00:28:06 +0200

    Creando listado de imagenes

--- src/components/Imagen.js
+++ src/components/Imagen.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const Imagen = ({imagen}) => {
-    return ( <p>imagen</p> );
+    
+    // Extraer las variables 
+    const { largeImageURL, likes, previewURL, tags, views  } = imagen;
+
+    return (
+        <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+            <div className="card">
+                <img src={previewURL} alt={tags} className="card-img-top"/>
+            </div>
+        </div>
+    );
 }
  
 export default Imagen;
